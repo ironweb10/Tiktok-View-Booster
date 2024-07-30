f987a155bf9994a435b825e5cc6a1421b9aa819d
ironweb10 <97882381+ironweb10@users.noreply.github.com>
2024-07-30 13:50:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ except ModuleNotFoundError:
     os.system('pip install colorama')
     os.system('pip install requests')
     os.system('pip install datetime')
+    from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+  return "Your Bot Is Ready"
+
+def run():
+  app.run(host="0.0.0.0", port=8000)
+
+def keep_alive():
+  server = Thread(target=run)
+  server.start()
 
 class Output:
     reset = Fore.RESET
